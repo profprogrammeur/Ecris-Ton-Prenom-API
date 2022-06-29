1454310ca63465f481fdf0cc7da817e7b37dbda8
RIEULoic <lrieu31@outlook.fr>
2022-06-29 17:24:20 +0400

    add method and button to delete account

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/member-data', to: 'members#show'
 
   match '/member-update', to: 'members#update', via: %i[patch put]
+  match '/member-delete', to: 'members#delete', via: %i[delete]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
